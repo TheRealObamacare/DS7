4fae8ab69ea08642d95388275ec96736350d7d2a
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2024-12-11 16:13:33 -0600

    yay

--- src/DS7_Searches.java
+++ src/DS7_Searches.java
@@ -1,2 +1,33 @@
 public class DS7_Searches {
+    public static int linearSearch(int[] arr, int target) {
+        int ans = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+    public static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end)
+        {
+            int mid = (start + end) / 2;
+            if(arr[mid] == target)
+            {
+                return mid;
+            }
+            else if(arr[mid] < target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
--- src/DS7_Sorts.java
+++ src/DS7_Sorts.java
@@ -1,2 +1,92 @@
 public class DS7_Sorts {
+    public static void selectionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    public static void insertionSort(int[] arr)
+    {
+        for (int i = 1; i < arr.length; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    public static void mergeSort(int[] data, int from, int to)
+    {
+        int[] tempData = new int[data.length];
+        if(to == from)
+        {
+            return;
+        }
+        int middle = (from + to) / 2;
+        mergeSort(data, from, middle);
+        mergeSort(data, middle + 1, to);
+        int i = from;
+        int j = middle + 1;
+        int k = from;
+        while (i <= middle && j <= to)
+        {
+            if (data[i] < data[j])
+                tempData[k++] = data[i++];
+            else
+                tempData[k++] = data[j++];
+        }
+        while (i <= middle)
+            tempData[k++] = data[i++];
+        while (j <= to)
+            tempData[k++] = data[j++];
+        for (k = from; k <= to; k++)
+            data[k] = tempData[k];
+    }
+    public static void quickSort(int[] data, int from, int to)
+    {
+        if (from >= to)
+            return;
+        int p = data[(from + to) / 2]; // Choose pivot as the middle element
+        int i = from;
+        int j = to;
+        while (i <= j)
+        {
+            while (data[i] < p) i++;
+            while (data[j] > p) j--;
+            if (i <= j)
+            {
+                int temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (from < j)
+            quickSort(data, from, j);
+        if (i < to)
+            quickSort(data, i, to);
+    }
+    public static void heapSort(int[] data)
+    {
+        for(int i = 0; i < data.length; i++)
+        {
+            
+        }
+    }
 }
